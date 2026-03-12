a98a63162a20585aaeeeb7fc9f3e94ff31f80d64
Prajwalgopi <146107257+Prajwalgopi@users.noreply.github.com>
2026-03-12 19:48:21 +0530

    Update my_first_dbt_model.sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -4,11 +4,11 @@
 
 with source_data as (
 
-    select 1 as id, 'active' as status
+    select 1 as id, 'inactive' as status
     union all
-    select 2 as id, 'inactive' as status
+    select 2 as id, 'active' as status
     union all
-    select 3 as id, 'active' as status
+    select 3 as id, 'inactive' as status
 
 )
 
